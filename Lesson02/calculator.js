6e182424b82084df1684f18bb870fe7c80b860d1
ChrisDelaPena <delapc@gmail.com>
2020-01-23 00:23:29 -0800

    Created calculator.js and countdown.js

---
+++ Lesson02/calculator.js
@@ -0,0 +1,12 @@
+let w = 2;
+let x = 5;
+let y = 10;
+let z = 3000;
+
+console.log(w + x + y);
+console.log(Math.pow(x,w));
+console.log(y % x);
+console.log(w % x);
+console.log(z--);
+console.log(x / y);
+console.log((-z / y));
